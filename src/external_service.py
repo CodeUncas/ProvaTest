0592499d82d65588319e5ef51d3c6b6d806fc957
CodeUncas <peruzziuncas@gmail.com>
2025-03-01 15:25:50 +0100

    dnsoauihdu9a

---
+++ src/external_service.py
@@ -0,0 +1,16 @@
+''''
+Modulo
+'''
+class Nonso:
+    ''''Clase'''
+
+    def __init__(self):
+        '''Constructor'''
+        self.a= 5
+
+    def metodo(self):
+        '''Metodo'''
+        return self.a
+    def metodo2(self):
+        '''Metodo'''
+        return self.a
