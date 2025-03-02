d43ce7508c7233b88ab53c8ed76dc7e14a629e05
CodeUncas <peruzziuncas@gmail.com>
2025-03-02 17:28:55 +0100

    Push

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,8 +1,11 @@
 import pytest
 from ..SimulatePositions.provaTest import fakefunc
-
+from ..SimulatePositions.provaTest import anotherfakefunc  
 # FILE: SimulatePositions/test_provaTest.py
 
 
 def test_fakefunc():
     assert fakefunc() == 1
+
+def test_anotherfakefunc():
+    assert anotherfakefunc() == 2
